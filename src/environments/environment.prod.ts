e3766c0de93c6d6455f35405280310088135501a
EzequielOmar <ezequiel.omar.vazquez@gmail.com>
2021-10-02 11:19:20 -0300

    preparando environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAj1W7ewAdmJjKqZE_X7xqjN8CZhNC4tQs',
+    authDomain: 'spp-laboiv.firebaseapp.com',
+    projectId: 'spp-laboiv',
+    storageBucket: 'spp-laboiv.appspot.com',
+    messagingSenderId: '1087147527722',
+    appId: '1:1087147527722:web:ac03932a742207a15c7d53',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAj1W7ewAdmJjKqZE_X7xqjN8CZhNC4tQs",
+    authDomain: "spp-laboiv.firebaseapp.com",
+    projectId: "spp-laboiv",
+    storageBucket: "spp-laboiv.appspot.com",
+    messagingSenderId: "1087147527722",
+    appId: "1:1087147527722:web:ac03932a742207a15c7d53"
+  }
 };
 
 /*
